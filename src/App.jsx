961b76b6d10c076b955920649fd4ef5dad405b22
Shanafkhan <shanaf.khan@blenheimchalcot.com>
2025-01-08 10:38:07 +0530

    made the button as a seperate component, also added conditional rendering of the input and the no project added component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import ProjectSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { preconnect } from "react-dom";
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartProject} />
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,15 +1,20 @@
-import noProjectImage from '../assets/no-projects.png'
-export default function NoProjectSelected(){
-    return(
-        <div>
-            <img src={noProjectImage} alt="An empty project list" className='w-16 h-16 object-contain mx-auto'/>
-            <h2 className='text-xl font-bold text-stone-500 my-4'>No project Selected</h2>
-            <p className='text-stone-400 mb-4'>Select a project to get started</p>
-            <p className='mt-8'>
-                <button>
-                    Create a new Project
-                </button>
-            </p>
-        </div>
-    );
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button.jsx";
+export default function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt="An empty project list"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">Select a project to get started</p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create a new Project</Button>
+      </p>
+    </div>
+  );
 }
