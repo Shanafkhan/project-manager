3bed0c09a36134762b22383c7e63e93947e33494
ShanafKhan <shanafkhan4@gmail.com>
2025-01-09 07:52:16 +0530

    created a feature to add the projects

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,5 +1,22 @@
+import { useRef } from "react";
 import Input from "./Input";
-export default function NewProject() {
+export default function NewProject({onAdd}) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave(){
+    const eneteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: eneteredTitle,
+      description : enteredDescription,
+      duedate : enteredDueDate
+    });
+
+  }
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -7,13 +24,15 @@ export default function NewProject() {
           <button className="text-stone-800 hover:text-stone-950 ">Cancel</button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
+            Save
+        </button>
         </li>
       </menu>
       <div>
-        <Input label="title" />
-        <Input label="Description" textarea />
-        <Input label="Due date" />
+        <Input ref={title} label="title" />
+        <Input ref={description} label="Description" textarea />
+        <Input ref={dueDate} label="Due date" />
       </div>
     </div>
   );
