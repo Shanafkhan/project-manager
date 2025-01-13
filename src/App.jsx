b362b7a9939721fcf2e5bbd1e14faca2fb89ab1d
ShanafKhan <shanafkhan4@gmail.com>
2025-01-13 07:23:39 +0530

    Created to new modal to handle the input validation using imerativehandle forwardref also added a function to handle cancel button

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,14 @@ function App() {
     });
   }
 
+  function handleCancelProject(){
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
   function handleAddProject(projectData){
     setProjectState((prevState) =>{
       const newProject = {
@@ -36,7 +44,7 @@ function App() {
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject}/>;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject}/>;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartProject} />;
   }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,27 +1,44 @@
 import { useRef } from "react";
 import Input from "./Input";
-export default function NewProject({onAdd}) {
+import Modal from "./Modal";
+export default function NewProject({onAdd, onCancel}) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+  const modal = useRef();
 
   function handleSave(){
-    const eneteredTitle = title.current.value;
+    const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
+    if(
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === '' 
+    ){
+      modal.current.open();
+      return;
+    }
+
     onAdd({
-      title: eneteredTitle,
+      title: enteredTitle,
       description : enteredDescription,
       duedate : enteredDueDate
     });
 
   }
   return (
+    <>
+    <Modal ref={modal} buttonCaption={'Okay'}>
+      <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+      <p className="text-stone-600 mb-4">oops.. looks like you entered a invalid input</p>
+      <p className="text-stone-600 mb-4">please make sure you enter a valuable input</p>
+    </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950 ">Cancel</button>
+          <button className="text-stone-800 hover:text-stone-950 " onClick={onCancel}>Cancel</button>
         </li>
         <li>
           <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
@@ -35,5 +52,6 @@ export default function NewProject({onAdd}) {
         <Input type="date" ref={dueDate} label="Due date" />
       </div>
     </div>
+    </>
   );
 }
